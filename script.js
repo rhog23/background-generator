1293a05118bbd1187cd57809d676a321a0e3b705
Raymond Saragih <raymonde.saragih@gmail.com>
2019-07-22 14:22:53 +0700

    adding npm lodash

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var _ = require("lodash");
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log('answer:', _.without(array, 3));
+
+
 let css = document.querySelector("h3");
 let color1 = document.querySelector("#color1");
 let color2 = document.querySelector("#color2");
@@ -7,7 +13,7 @@ function setGradient() {
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-    css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
